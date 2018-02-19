74ce861dc3e0ba459898de84b4a17b91c4f32960
Ryan Smith <rnsmith2@gmail.com>
2018-02-19 09:42:34 -0800

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yummly/logentries-timbre-appender "0.1.0-SNAPSHOT"
+(defproject yummly/logentries-timbre-appender "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name         "Eclipse Public License"
