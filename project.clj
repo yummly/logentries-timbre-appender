cc3a03d5a80574ec0f1dd30cc55a5825b318e5bd
Ryan Smith <rnsmith2@gmail.com>
2018-08-16 14:32:16 -0700

    Set to next snapshot version: 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yummly/logentries-timbre-appender "0.1.1"
+(defproject yummly/logentries-timbre-appender "0.1.2-SNAPSHOT"
   :description "Timbre appender for logentries.com"
   :url "http://example.com/FIXME"
   :license {:name         "Eclipse Public License"
